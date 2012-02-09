5584101c2b705520aeec39833958af870da51d94
Colin Burn-Murdoch <colin@burn-murdoch.com>
2012-02-09 08:47:02 -0800

    Changed Route:get()->uri() calls to shorter Route::url()

--- views/pagination/render.php
+++ views/pagination/render.php
@@ -4,13 +4,13 @@
 		<?php if ($page == 1): ?>
 			<li class="prev disabled"><a>&larr; Previous</a></li>
 		<?php else: ?>
-			<li class="prev"><a href="<?php echo URL::site(Route::get($route)->uri(array_merge($route_params, array($route_page_param => $page-1)))); ?>">&larr; Previous</a></li>
+			<li class="prev"><a href="<?php echo Route::url($route, array_merge($route_params, array($route_page_param => $page-1))); ?>">&larr; Previous</a></li>
 		<?php endif; ?>
 		
 		<?php if ($pages <= 1): ?>
 			<li class="disabled"><a>1</a></li>
 		<?php else: ?>
-			<li <?php if ($page == 1): ?>class="active"<?php endif; ?>><a href="<?php echo URL::site(Route::get($route)->uri(array_merge($route_params, array($route_page_param => 1)))); ?>">1</a></li>
+			<li <?php if ($page == 1): ?>class="active"<?php endif; ?>><a href="<?php echo Route::url($route, array_merge($route_params, array($route_page_param => 1))); ?>">1</a></li>
 
 			<?php
 				$start_page = $page - ($show_max_pages) + 1;
@@ -24,19 +24,19 @@
 			<?php if ($start_page > 2): ?> <li><a>...</a></li><?php endif; ?>
 
 			<?php for($i=$start_page; $i<=$end_page; $i++): ?>
-				<li <?php if ($page == $i): ?>class="active"<?php endif; ?>><a href="<?php echo URL::site(Route::get($route)->uri(array_merge($route_params, array($route_page_param => $i)))); ?>"><?php echo $i; ?></a></li>
+				<li <?php if ($page == $i): ?>class="active"<?php endif; ?>><a href="<?php echo Route::url($route, array_merge($route_params, array($route_page_param => $i))); ?>"><?php echo $i; ?></a></li>
 			<?php endfor; ?>
 
 			<?php if ($end_page < $pages-1): ?> <li><a>...</a></li><?php endif; ?>
 
-			<li <?php if ($page == $pages): ?>class="active"<?php endif; ?>><a href="<?php echo URL::site(Route::get($route)->uri(array_merge($route_params, array($route_page_param => $pages)))); ?>"><?php echo $pages; ?></a></li>
+			<li <?php if ($page == $pages): ?>class="active"<?php endif; ?>><a href="<?php echo Route::url($route, array_merge($route_params, array($route_page_param => $pages))); ?>"><?php echo $pages; ?></a></li>
 
 		<?php endif; ?>
 
 		<?php if ($page == $pages || $pages == 0): ?>
 			<li class="next disabled"><a>Next &rarr;</a></li>
 		<?php else: ?>
-			<li class="next"><a href="<?php echo URL::site(Route::get($route)->uri(array_merge($route_params, array($route_page_param => $page+1)))); ?>">Next &rarr;</a></li>
+			<li class="next"><a href="<?php echo Route::url($route, array_merge($route_params, array($route_page_param => $page+1))); ?>">Next &rarr;</a></li>
 		<?php endif; ?>
 
 	</ul>
@@ -44,7 +44,7 @@
 	<ul class="per-page">
 		<li class="prev disabled"><a>Per page:</a></li>
 		<?php foreach ($per_page_choices as $i => $choice): ?>
-		<li class="<?php if ($i == count($per_page_choices)-1): ?>next<?php endif; ?> <?php if ($per_page==$choice): ?>active<?php endif; ?>"><a href="<?php echo URL::site(Route::get($route)->uri(array_merge($route_params, array($route_page_param => 1, 'per_page' => $choice)))); ?>"><?php echo $choice; ?></a></li>
+		<li class="<?php if ($i == count($per_page_choices)-1): ?>next<?php endif; ?> <?php if ($per_page==$choice): ?>active<?php endif; ?>"><a href="<?php echo Route::url($route, array_merge($route_params, array($route_page_param => 1, 'per_page' => $choice))); ?>"><?php echo $choice; ?></a></li>
 		<?php endforeach; ?>
 	</ul>
 
